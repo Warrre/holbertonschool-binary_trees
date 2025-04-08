f9dd544eba9f944bb61b53a95ce0631f43a8a6d3
warrre <warren.soulsey@gmail.com>
2025-04-08 11:07:21 +0200

    Implement binary tree leaf node check function and update header file

---
+++ 4-binary_tree_is_leaf.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "binary_trees.h"
+
+/**
+ * binary_tree_is_leaf - Checks if a node is a leaf (has no children)
+ * @node: Pointer to the node to check
+ *
+ * Return: 1 if the node is a leaf, 0 otherwise or if node is NULL
+ */
+int binary_tree_is_leaf(const binary_tree_t *node)
+{
+    if (node == NULL)
+        return (0);
+
+    if (node->left == NULL) 
+        return (1);
+        
+    if (node->right == NULL)
+        return (1);    
+    
+    else
+    
+    return (0);
+}
